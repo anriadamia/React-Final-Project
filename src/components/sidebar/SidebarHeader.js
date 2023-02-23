df14e24646d655e88557729b4b02764681dc30c5
anriadamia <anriadamia2000@gmail.coom>
2023-02-23 21:51:47 +0400

    Final Project

--- src/components/sidebar/SidebarHeader.js
+++ src/components/sidebar/SidebarHeader.js
@@ -1,14 +1,21 @@
 import { Box, styled } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
+import pic from "./Image/Logo.webp";
 
 const StyledHeader = styled(Box)(() => ({
   padding: "15px 15px",
-  background: "teal",
-  height:"64px",
-  display:"flex",
-  alignItems:"center",
+  height: "64px",
+  display: "flex",
+  alignItems: "center",
 }));
 
 export const SidebarHeader = () => {
-  return <StyledHeader>SomeLogo</StyledHeader>;
+  return (
+    <StyledHeader marginTop="10px">
+      <Link to="/">
+        <img src={pic}   style={{ objectFit: "contain", borderRadius:"10px", height:"100px", width:"100%" }}   />
+      </Link>
+    </StyledHeader>
+  );
 };
