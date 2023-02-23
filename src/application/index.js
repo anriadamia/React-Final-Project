54cd44ef1f4fb6197ff68ece6908157dc93304cd
anriadamia <anriadamia2000@gmail.coom>
2023-02-23 09:54:41 +0400

    Final Project

--- src/application/index.js
+++ src/application/index.js
@@ -1,3 +1,5 @@
 export {axiosInstance} from "./instance";
 export {getUserInitials, isUserAdmin} from "./utilis";
 export {ProtectedRoute} from "./ProtectedRoute";
+export {useForm} from "./hook/useForm";
+export {useQueryParams} from "./hook/useQueryParams"
--- src/components/product/categoryProducts/index.js
+++ src/components/product/categoryProducts/index.js
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useQueryParams } from "../../../application";
 import { fetchCategoryProducts, useCategoryProducts } from "../../../redux";
 import { CategoryProductList } from "./CategoryProductList";
 import { Paginate } from "./Paginate";
@@ -10,15 +11,17 @@ import { Sort } from "./Sort";
 export const CategoryProducts = () => {
   const { categoryName } = useParams();
   const dispatch = useDispatch();
-  const { products} = useCategoryProducts();
+  const { products } = useCategoryProducts();
+  const { value:sort, changeQueryValue: changeSort } = useQueryParams("sort");
+  const { value:page, changeQueryValue: changePage } = useQueryParams("page");
   useEffect(() => {
     dispatch(
-      fetchCategoryProducts(`${categoryName}?page=1&size=3&sort=name,asc`)
+      fetchCategoryProducts(`${categoryName}?page=1&size=3&sort=${sort}`)
     );
-  }, [categoryName]);
+  }, [categoryName, sort]);
   return (
     <Box>
-      <Sort />
+      <Sort sort={sort} changeSort={changeSort}/>
       <CategoryProductList products={products} />
       <Paginate />
     </Box>
