27f1e05dd15211b7eb28285f1e0a0a0a3f42a896
anriadamia <anriadamia2000@gmail.coom>
2023-02-24 22:15:01 +0400

    Final Project

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -9,7 +9,7 @@ import {
 } from "@mui/material";
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
-import { useCategories, useProductLoading } from "../../redux";
+import { useCategories } from "../../redux";
 import { SidebarHeader } from "./SidebarHeader";
 
 const StyledListItem = styled(ListItem)(() => ({
@@ -18,7 +18,6 @@ const StyledListItem = styled(ListItem)(() => ({
 }));
 
 export const Sidebar = () => {
-  const isSideBarLoading=useProductLoading();
   const sidebarItems = useCategories();
   return (
     <Drawer
